9b58dfb84521369cf126c4cc58a5eeff8aa82e44
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2022-07-29 14:26:46 -0600

    remove all unessesary files, add assets from wireframe, add files for app file structure

--- mae-blog/src/App.js
+++ mae-blog/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Navbar from './Components/Navbar'
+import {
+  BrowserRouter,
+  Routes, 
+  Route} from 'react-router-dom';
+import Home from './Components/Pages/Home';
+import NewPost from './Components/Pages/NewPost';
+import Profile from './Components/Pages/Profile';
+import Footer from './Components/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Profile' element={<Profile/>}/>
+          <Route path='/New-Post' element={<NewPost/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ mae-blog/src/Components/navbar.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from 'react';
+import './Navbar.css';
+// import { Link } from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+
+  return (
+  <>
+    <nav className='navbar' id='/#'>
+        <div className="navbar-container">
+            <Link to='/#' className='navbar-logo' onClick={closeMobileMenu}>
+                <i class="fas fa-record-vinyl"></i>
+            </Link>
+            <div className='menu-icon' onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'> 
+                    <Link to='/background' className='nav-links' onClick={closeMobileMenu}>
+                        About Me
+                    </Link>
+                </li>
+                <li className='nav-item'> 
+                    <Link to='/#projects' className='nav-links' onClick={closeMobileMenu}>
+                        Projects
+                    </Link>
+                </li>
+                <li className='nav-item'> 
+                    <Link to='/#contact' className='nav-links' onClick={closeMobileMenu}>
+                        Contact Me
+                    </Link>
+                </li>
+                <li className='nav-item'> 
+                    <Link to='/#links' className='nav-links' onClick={closeMobileMenu}>
+                        Links
+                    </Link>
+                </li>  
+            </ul>
+        </div>
+    </nav>
+  </>
+  )
+}
+
+export default Navbar;
